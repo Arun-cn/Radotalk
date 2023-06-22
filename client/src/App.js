3227c306d36bb9359109a96ec2952906dd809e3f
Arun-cn <arunchandran4321@gmail.com>
2023-06-22 06:47:30 +0000

    add routes and home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-
+import { Route,Routes } from 'react-router-dom';
+import Home from "./pages/Home"
 function App() {
   return (
-    <div className="App">
-      <h1>Radotlak</h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+
+
+
+
+    </Routes>
   );
 }
 
