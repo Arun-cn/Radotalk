631b20868a0c570537b9e928f0c370707b7e4bac
Arun-cn <arunchandran4321@gmail.com>
2023-06-26 15:37:49 +0000

    add register navigation

--- client/src/components/layout/Headers.js
+++ client/src/components/layout/Headers.js
@@ -1,13 +1,18 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 const Headers = () => {
+  const navigate = useNavigate()
+  const handileClick = (e)=>{
+    e.preventDefault();
+    navigate('/register');
+  }
   return (
     <div>
 
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">Radotalk</a>
-    <button className="btn">Register
+    <button className="btn" onClick={handileClick}>Register
       
     </button>
     
