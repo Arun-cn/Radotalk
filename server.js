35541b74cb2b3ee8acc0233ebbf9953401fa6c7c
Arun-cn <arunchandran4321@gmail.com>
2023-06-26 13:52:42 +0000

    add socket io setup

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 
 const express = require('express');
 const app = express();
-
 const http = require('http');
+const server =  http.createServer(app);
+const {Server} = require('socket.io')
+const io = new Server(server);
 const connectDB  = require('./config/db');
 const router = require('./routes/authRoute');
-const server =  http.createServer(app);
+
 
 app.use(express.json());
 
@@ -15,6 +17,12 @@ app.get('/',(req,res)=>{
 
 app.use("/api/v1/auth",router);
 connectDB();
-app.listen(3000,()=>{
-    console.log('server started in 3000');
-})
+
+io.on('connection',(server)=>{
+  console.log('user connected');
+});
+
+
+server.listen(3000,()=>{
+    console.log('server started 3000')
+});
