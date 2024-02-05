d841542eb2950adb21610710e328f72bef2b3af7
Arun-cn <arunchandran4321@gmail.com>
2024-02-05 20:21:46 +0530

    fix mogodb conection and error

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connectDB = async () => {
-    console.log(process.env.MONGODB_URL);
-    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    
+    
     try{
+        const conn = await mongoose.connect(`${process.env.MONGODB_URL}/radotalk`);
         console.log('connected to mongo db');
     } catch (error) {
         console.log('error to conect mongo db');
