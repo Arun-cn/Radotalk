792245e1df6b3c726a6c1904f23aa2647a8267ef
Arun-cn <arunchandran9876@gmail.com>
2024-08-29 21:31:42 +0530

    chore: add ESLint and Prettier for code linting and formatting

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      "prettier/prettier": "error", // Ensure Prettier errors are flagged by ESLint
+    },
+  },
+  pluginJs.configs.recommended,
+  prettierConfig, // Disable ESLint rules that would conflict with Prettier
+];
